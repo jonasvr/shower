4f60177ece9f2ae4f1413c157fef0846f3692fe6
root <jonasvanreeth@gmail.com>
2016-08-23 12:31:10 +0200

    middelware web

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,61 +15,61 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::auth();
+Route::group( [ 'middleware' => ['web'] ], function () {
+    Route::auth();
 
-Route::get('/home', 'HomeController@index');
-Route::group(['prefix' => 'info'],function(){
-    Route::get('/', ['as'=>'getInfo', 'uses' => 'ProfileController@register']);
-//    Route::post('/add', ['as'=>'addInfo', 'uses' => 'ProfileController@addInfo']);
-    Route::post('/addKot', ['as'=>'addKot', 'uses' => 'KotController@addKot']);
-});
-
-
-Route::group(['prefix' => 'profile'], function(){
-    Route::get('/', ['as'=>'getProfile', 'uses' => 'ProfileController@getProfile']);
-    Route::post('/addDevice', ['as'=>'addDevice', 'uses' => 'KotController@addDevice']);
-    Route::get('/delete/{id}', ['as'=>'delete', 'uses' => 'KotController@deleteHabitant']);
-    Route::post('/add', ['as'=>'addInfo', 'uses' => 'ProfileController@addInfo']);
-    Route::post('/add2', ['as'=>'addInfo2', 'uses' => 'ProfileController@addInfo2']);
-    Route::post('/edit/post', ['as'=>'editpost', 'uses' => 'ProfileController@addInfo2']);
-    Route::post('/info',['as' => 'info','uses' => 'ProfileController@addInfo']);
-    Route::get('/edit/', ['as'=>'editPicture', 'uses' => 'ProfileController@getedit']);
-
-    Route::get('/crop',['as' => 'crop', function()
-    {
-        return View('profile.functions.crop')->with('image', 'img/users/'. Session::get('image'));
-    }]);
-
-    Route::post('/postcrop', ['as' => 'postCrop', 'uses' => 'PhotoController@postCrop']);\
-
-    Route::group(['prefix'=>'calendar'],function(){
-            Route::get('/{id}',['as'=>'calendar','uses'=>'CalendarController@Calendar']);
-            Route::post('/reserve',['as'=>'reserve','uses'=>'CalendarController@reserve']);
-            Route::get('cancel/{id}',['as'=>'cancel','uses'=>'CalendarController@cancel']);
-        });
-});
-
-Route::group(['prefix' => 'admin'], function(){
-    Route::get('/',['as' => 'admin', 'uses' => 'KotController@admin']);
-    Route::get('/accept/{id}', ['as'=>'accept', 'uses' => 'KotController@accept']);
-    Route::get('/decline/{id}', ['as'=>'decline', 'uses' => 'KotController@decline']);
-    Route::get('/broken/{id}', ['as'=>'broken', 'uses' => 'KotController@broken']);
-    Route::get('/edit/{id}', ['as'=>'editName', 'uses' => 'KotController@getedit']);
-    Route::post('/edit/', ['as'=>'editDevice', 'uses' => 'KotController@editDevice']);
-});
-
-Route::group(['prefix' => 'api','middleware' => 'api'], function () {
-    Route::get('shower/{device_id}/{state}', 'ApiController@showerGet');
-    Route::get('state/{koten_id}','ApiController@changeState');
-    Route::get('calstate/{device_id}','ApiController@changecalState');
-});
+    Route::get('/home', 'HomeController@index');
+    Route::group(['prefix' => 'info'], function () {
+        Route::get('/', ['as' => 'getInfo', 'uses' => 'ProfileController@register']);
+        Route::post('/addKot', ['as' => 'addKot', 'uses' => 'KotController@addKot']);
+    });
 
 
+    Route::group(['prefix' => 'profile'], function () {
+        Route::get('/', ['as' => 'getProfile', 'uses' => 'ProfileController@getProfile']);
+        Route::post('/addDevice', ['as' => 'addDevice', 'uses' => 'KotController@addDevice']);
+        Route::get('/delete/{id}', ['as' => 'delete', 'uses' => 'KotController@deleteHabitant']);
+        Route::post('/add', ['as' => 'addInfo', 'uses' => 'ProfileController@addInfo']);
+        Route::post('/add2', ['as' => 'addInfo2', 'uses' => 'ProfileController@addInfo2']);
+        Route::post('/edit/post', ['as' => 'editpost', 'uses' => 'ProfileController@addInfo2']);
+        Route::post('/info', ['as' => 'info', 'uses' => 'ProfileController@addInfo']);
+        Route::get('/edit/', ['as' => 'editPicture', 'uses' => 'ProfileController@getedit']);
 
-Route::get('/stats',['as'=>'stats', 'uses' => 'HomeController@stats']);
+        Route::get('/crop', ['as' => 'crop', function () {
+            return View('profile.functions.crop')->with('image', 'img/users/' . Session::get('image'));
+        }]);
 
-route::get('/send','HomeController@send');
+        Route::post('/postcrop', ['as' => 'postCrop', 'uses' => 'PhotoController@postCrop']);
+        \
 
-route::get('/mail',function(){
-    return view('mail.mailstyle');
+        Route::group(['prefix' => 'calendar'], function () {
+                Route::get('/{id}', ['as' => 'calendar', 'uses' => 'CalendarController@Calendar']);
+                Route::post('/reserve', ['as' => 'reserve', 'uses' => 'CalendarController@reserve']);
+                Route::get('cancel/{id}', ['as' => 'cancel', 'uses' => 'CalendarController@cancel']);
+            });
+    });
+
+    Route::group(['prefix' => 'admin'], function () {
+        Route::get('/', ['as' => 'admin', 'uses' => 'KotController@admin']);
+        Route::get('/accept/{id}', ['as' => 'accept', 'uses' => 'KotController@accept']);
+        Route::get('/decline/{id}', ['as' => 'decline', 'uses' => 'KotController@decline']);
+        Route::get('/broken/{id}', ['as' => 'broken', 'uses' => 'KotController@broken']);
+        Route::get('/edit/{id}', ['as' => 'editName', 'uses' => 'KotController@getedit']);
+        Route::post('/edit/', ['as' => 'editDevice', 'uses' => 'KotController@editDevice']);
+    });
+
+    Route::group(['prefix' => 'api', 'middleware' => 'api'], function () {
+        Route::get('shower/{device_id}/{state}', 'ApiController@showerGet');
+        Route::get('state/{koten_id}', 'ApiController@changeState');
+        Route::get('calstate/{device_id}', 'ApiController@changecalState');
+    });
+
+
+    Route::get('/stats', ['as' => 'stats', 'uses' => 'HomeController@stats']);
+
+    route::get('/send', 'HomeController@send');
+
+    route::get('/mail', function () {
+        return view('mail.mailstyle');
+    });
 });
