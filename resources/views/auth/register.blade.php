657eac66b20b2fa17c37fa0deaa53b08d87d8a08
root <jonasvanreeth@gmail.com>
2016-08-23 11:46:09 +0200

    no changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -51,21 +51,6 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-
                         <div class="form-group">
                             <label for="admin" class="col-md-4 control-label">New Kot</label>
 
